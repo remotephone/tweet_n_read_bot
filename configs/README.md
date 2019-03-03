051d62a9ed6367472b9232367032050567d404e1
computer <remotephone@gmail.com>
2019-03-03 17:39:23 -0600

    more bits

--- configs/README.md
+++ configs/README.md
@@ -1,3 +1,5 @@
 ## Configs
 
 Add your twitter API key, queries will be a list of whatever topics you're into.
+
+The config.cfg.sample can be copied and renamed config.cfg 
--- safety_check.py
+++ safety_check.py
@@ -19,24 +19,42 @@ def slow_down(func):
 class VirusTotaler:
     def __init__(self, cfg):
         self.cfg = cfg
+        apikey = self.cfg['virustotal']['apikey']
 
+    def process_url(self, url):
+        pass
 
 
     def vt_get(self, url):
 
         apikey = self.cfg['virustotal']['apikey']
-
-        vtendpt = 'https://www.virustotal.com/vtapi/v2/url/report'
+        getendpt = 'https://www.virustotal.com/vtapi/v2/url/report'
         params = {'apikey': apikey, 'resource': url}
 
 
-        response = requests.get(vtendpt, params=params)
+        response = requests.get(getendpt, params=params)
         response = response.json()
 
-        if response['response_code'] == 0:
-            pass
+        if response['response_code'] == -2:
+            logger.info('[!] - Url {} not ready, sleeping'.format(url)
+            sleep(5)
+            return url_tocheck
+
         print(response)
         print(response['positives'])
 
+    def vt_put(self, url):
+        url_tocheck = self.
+        
+
+        putendptl = 'https://www.virustotal.com/vtapi/v2/url/scan'
+
+        params = {'apikey': '<apikey>', 'url': url }
+
+        response = requests.post(url, data=params)
+
+        print(response.json())
+
 
 # vt_get('http://www.google23r432rf2.com')
+
